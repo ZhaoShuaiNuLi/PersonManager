1d5dfadf1a5ca8a830fe7814b54838228ad0c954
ZhaoShuaiNuLi <zhaoshuai0822@163.com>
2019-07-19 15:45:30 +0800

    第三次测试

--- src/main/java/com/cy/pj/PersonManagerApplication.java
+++ src/main/java/com/cy/pj/PersonManagerApplication.java
@@ -2,7 +2,7 @@ package com.cy.pj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//ceshi
 @SpringBootApplication
 public class PersonManagerApplication {
 	public static void main(String[] args) {
