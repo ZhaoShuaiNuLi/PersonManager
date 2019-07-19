8e91b46bc71c4c2f82bddd3671651d202b9c7555
ZhaoShuaiNuLi <zhaoshuai0822@163.com>
2019-07-19 14:51:33 +0800

    测试取消

--- src/main/java/com/cy/pj/PersonManagerApplication.java
+++ src/main/java/com/cy/pj/PersonManagerApplication.java
@@ -1,11 +1,10 @@
 package com.cy.pj;
-//做一下改变测试上传github
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonManagerApplication.class, args);
 	}
